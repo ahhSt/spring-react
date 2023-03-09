35d7d25ab37dd969e6048b3018e7a7c6e63a62c3
heeseung <heeseung83@straffic.co.kr>
2023-03-09 16:23:39 +0900

    react build

--- demo/src/main/java/com/example/demo/service/CustomerService.java
+++ demo/src/main/java/com/example/demo/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Customer;
+import com.example.demo.mapper.CustomerMapper;
 import com.example.demo.repository.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,31 +19,18 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    CustomerMapper customerMapper;
+
+    @Transactional(readOnly = true)
+    public List<Customer> listCusomter_mybatis() throws Exception{
+
+        List<Customer> list = (List<Customer>) customerMapper.listCustomer();
+        log.info(list.toString());
+        return list;
+    }
     @Transactional(readOnly = true)
     public List<Customer> listCustomer() throws Exception {
-
-//        System.out.println("\n1.findAll()...");
-//        for (Customer customer : customerRepository.findAll()) {
-//            System.out.println(customer);
-//        }
-//
-//        System.out.println("\n2.findByEmail(String email)...");
-//        for (Customer customer : customerRepository.findByEmail("222@yahoo.com")) {
-//            System.out.println(customer);
-//        }
-//
-//        System.out.println("\n3.findByDate(Date date)...");
-//        for (Customer customer : customerRepository.findByDate(sdf.parse("2017-02-12"))) {
-//            System.out.println(customer);
-//        }
-//
-//        // For Stream, need @Transactional
-//        System.out.println("\n4.findByEmailReturnStream(@Param(\"email\") String email)...");
-//        try (Stream<Customer> stream = customerRepository.findByEmailReturnStream("333@yahoo.com")) {
-//            stream.forEach(x -> System.out.println(x));
-//        }
-//
-//        System.out.println("Done!");
 
         List<Customer> list = (List<Customer>) customerRepository.findAll();
         log.info(list.toString());
