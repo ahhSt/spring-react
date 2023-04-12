faed0e5e58bc30ba6edc2c5473965abd8531a20d
heeseung <heeseung83@straffic.co.kr>
2023-04-12 17:25:25 +0900

    react list detail sample

--- demo/src/main/webapp/src/pages/sample/listInfoSample/index.js
+++ demo/src/main/webapp/src/pages/sample/listInfoSample/index.js
@@ -19,14 +19,11 @@ import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 import InputForm from './InputForm';
 
 const SearchBox = (props) => {
-  // const onChange = props.handleChange;
   const userInput = props.userInput;
   const setUserInput = props.setUserInput;
 
   const handleChange = ({target: {value}}) => {
-    console.log('handleChange');
     setUserInput(value.toLowerCase());
-    // ref.current.focus();
   }
 
   return (
@@ -41,11 +38,10 @@ SearchBox.displayName = "SearchBox";
 
 const SelectedListItem = (props) => {
 
-  console.log('SelectedListItemSelectedListItem');
-
+  const [userInput, setUserInput] = useState('');
   const customers = {...props.customers || {query:"", list:[]}};
-  const userInput = props.userInput;
-  const setUserInput = props.setUserInput;
+  // const userInput = props.userInput;
+  // const setUserInput = props.setUserInput;
   const selectedIndex = props.selectedIndex;
   const setSelectedIndex = props.setSelectedIndex;
 
@@ -54,15 +50,15 @@ const SelectedListItem = (props) => {
     return item.name.toLowerCase().includes(userInput);
   })
 
-  const ListItems = (props) => {
+  const ListItems = () => {
 
     const handleListItemClick = (event, item) => {
       setSelectedIndex(item.id);
     };
 
-    let listItems = props.items;
+    // let listItems = props.items;
 
-    const listItem = listItems.map((item, idx) => 
+    const listItem = selectedList.map((item, idx) => 
               <ListItemButton key={ item.id }
               selected={selectedIndex === item.id}
               onClick={(event) => handleListItemClick(event, item)}
@@ -76,83 +72,75 @@ const SelectedListItem = (props) => {
   
   return (
     <Box sx={{ width: '100%', maxwidth: 360, bgcolor: 'background.paper' }}>
-      {/* <SearchItem originCustomers={originCustomers} customers={customers} setCustomers={setCustomers}/> */}
       <SearchBox userInput={userInput} setUserInput={setUserInput} />
-      {/* <input onChange={handleChange}/> */}
-      <ListItems items={selectedList} setSelectId={props.setSelectId} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
+      <ListItems />
+      {/* <ListItems items={selectedList} setSelectId={props.setSelectId} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/> */}
     </Box>
   );
 };
 
 SelectedListItem.displayName = "SelectedListItem";
 
+function BoxComponent(props) {
+
+  const setSelectedIndex = props.setSelectedIndex;
+  const customers = props.customers;
+  const setCustomers = props.setCustomers;
+
+  function onAdd() {
+    // const lastCust = customers.reduce( (prev, current) => prev.id > current.id ? prev : current);
+    // const newId = lastCust.id + 1;
+    setCustomers({query: "new", list: [...customers.list, {id: "new", name:"New Data", isNew:true}]});
+    setSelectedIndex("new");
+  }
+
+  return (
+    <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Button variant="contained" startIcon={<PersonAddAltIcon />} onClick={onAdd}>
+        Add
+      </Button>
+    </Box>
+  );
+}
+
 export default function TestPage(){
 
-    const [originCustomers, setOriginCustomers] = useState(null);
     const [customers, setCustomers] = useState({query:'', list: []});
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [isNew, setIsNew] = useState(false);
-    // const [selectId, setSelectId] = useState(null);
-    const [userInput, setUserInput] = useState('');
     const [selectedIndex, setSelectedIndex] = useState(null);
-
-    // const nameInput = useRef();
 
     console.log('first');
 
     const fetchCustomers = async () => {
       try {
-        setError(null);
-        setCustomers({query: "", list: null});
+        // setError(null);
+        // setCustomers({query: "", list: []});
         // loading 상태를 true 로 바꿉니다.
-        setLoading(true);
+        // setLoading(true);
         const response = await axios.get(
             '/api/customer'
         );
-        setOriginCustomers(response.data); // 데이터는 response.data 안에 들어있습니다.
+        console.log("!!!!!!!!!!!!!!!!!Before main search");
         setCustomers({query:"", list: response.data}); // 데이터는 response.data 안에 들어있습니다.
 
+        console.log("!!!!!!!!!!!!!!!!!main search");
         let minId = response.data[0].id;
-        // setSelectedIndex(minId);
+        setSelectedIndex(minId);
       } catch (e) {
         setError(e);
       }
-      setLoading(false);
+      // setLoading(false);
     };
 
     useEffect(() => {
         fetchCustomers();
+        console.log("~~~~~~~~~~~~~~~~fetchCustomers");
       }, []);
 
     if (loading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
     if (!customers.list) return null;
-
-    // const handleListItemClick = (event, item) => {
-    //   console.log(item.isNew);
-    //   setIsNew(item.isNew);
-    //   setSelectedIndex(item.id);
-    // };
-
-    function BoxComponent() {
-        return (
-          <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
-            <Button variant="contained" startIcon={<PersonAddAltIcon />} onClick={onAdd}>
-              Add
-            </Button>
-          </Box>
-        );
-    }
-
-    function onAdd() {
-      console.log(customers);
-      // const lastCust = customers.reduce( (prev, current) => prev.id > current.id ? prev : current);
-      // const newId = lastCust.id + 1;
-      setCustomers({query: customers.query, list: [...customers.list, {id: null, name:"New Data", isNew:true}]});
-      setIsNew(true);
-      // setSelectedIndex(null);
-    }
 
     // const onChange = (e) => {
     //   setUserInput(e.target.value.toLowerCase());
@@ -162,15 +150,16 @@ export default function TestPage(){
         <Container maxwidth="sm">
             <Grid container spacing={2}>
                 <Grid xs={4}>
-                  <SelectedListItem userInput={userInput} setUserInput={setUserInput} customers={customers} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+                  <SelectedListItem customers={customers} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+                  {/* <SelectedListItem userInput={userInput} setUserInput={setUserInput} customers={customers} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} /> */}
                   {/* <SearchBox /> */}
                   {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange= {onChange} /> */}
                   {/* <ListItems items={ searched }/> */}
                   <Pagination count={10} color="primary" />
-                  <BoxComponent />
+                  <BoxComponent customers={customers} setCustomers={setCustomers} setSelectedIndex={setSelectedIndex}/>
                 </Grid>
                 <Grid xs={8}>
-                  <InputForm selectedIndex={selectedIndex} />
+                  <InputForm selectedIndex={selectedIndex} fetchCustomers={fetchCustomers}/>
                 </Grid>
             </Grid>
         </Container>
