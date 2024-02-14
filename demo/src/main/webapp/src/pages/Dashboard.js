3a32d673bd658019e034cedbe41358113f2ada84
heeseung83 <heeseung83@straffic.com>
2024-02-14 10:20:24 +0900

    clearing

--- demo/src/main/webapp/src/pages/Dashboard.js
+++ demo/src/main/webapp/src/pages/Dashboard.js
@@ -17,7 +17,7 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems, thirdListItems } from './listItems';
+import { mainListItems, secondaryListItems } from './listItems';
 import Routes from '../routes'
 // import Chart from './Chart';
 // import Deposits from './Deposits';
@@ -146,8 +146,6 @@ function DashboardContent() {
             {mainListItems}
             <Divider sx={{ my: 1 }} />
             {secondaryListItems}
-            <Divider sx={{ my: 1 }} />
-            {thirdListItems}
           </List>
         </Drawer>
         <Box
