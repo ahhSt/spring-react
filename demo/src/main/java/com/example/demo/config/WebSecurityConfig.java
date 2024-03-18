7f1559c0452c2aca487ed589ad559511a5ff5a7e
heeseung83 <heeseung83@straffic.com>
2024-03-18 10:29:38 +0900

    revert security

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,55 +1,18 @@
 package com.example.demo.config;
 
-import com.example.demo.filter.JwtAuthenticationFilter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class WebSecurityConfig {
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors().and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/api/")).permitAll()
-                .anyRequest().authenticated().and()
-                .exceptionHandling().authenticationEntryPoint(new FailedAuthenticationEntryPoint());
-
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-
-    }
-
-    class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
-        @Override
-        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-            response.setContentType("application/json");
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.getWriter().write("{ \"code\": \"NP\", \"message\": \"Do not have permission.\" }");
-        }
-
+        return http.cors().and().csrf().disable().authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
     }
 
 //    @Override
@@ -63,12 +26,12 @@ public class WebSecurityConfig {
 //        }
 //    }
     
-//	 @Bean
-//	 public Filter characterEncodingFilter() {
-//	 	CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//	 	filter.setEncoding("UTF-8");
-//	 	filter.setForceEncoding(true);
-//	 	return filter;
-//	 }
+	// @Bean
+	// public Filter characterEncodingFilter() {
+	// 	CharacterEncodingFilter filter = new CharacterEncodingFilter();
+	// 	filter.setEncoding("UTF-8");
+	// 	filter.setForceEncoding(true);
+	// 	return filter;
+	// }
 
 }
--- demo/src/main/resources/import.sql
+++ demo/src/main/resources/import.sql
@@ -2,6 +2,3 @@ INSERT INTO customer (ID, NAME, EMAIL, CREATED_DATE) VALUES(1, 'Sonny','Sonny@St
 INSERT INTO customer (ID, NAME, EMAIL, CREATED_DATE) VALUES(2, 'Sam','Sam@Straffic.com', TO_DATE('2024-02-12', 'yyyy-mm-dd'));
 INSERT INTO customer (ID, NAME, EMAIL, CREATED_DATE) VALUES(3, 'Kyle','Kyle@Straffic.com', TO_DATE('2024-02-13', 'yyyy-mm-dd'));
 INSERT INTO customer (ID, NAME, EMAIL, CREATED_DATE) VALUES(4, 'John','John@Straffic.com', TO_DATE('2024-02-13', 'yyyy-mm-dd'));
-
-INSERT INTO quality_tool (ID, serial_no,tool_maker,tool_model,tool_name,first_register,register_date, last_calibration_date, last_calibration_org)
-    VALUES (1, '31350132WS', 'FLUKE', '17B+', 'DIGITAL MULTIMETER', '23280', TO_DATE('2024-03-14', 'yyyy-mm-dd'), TO_DATE('2024-03-14', 'yyyy-mm-dd'), '교정기술원(주)');
